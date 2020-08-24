356105a19e287aa65f3accf4e00b6f31790a0a36
FreekAlberti <federicoalberi96@gmail.com>
2020-08-24 15:44:00 +0200

    esercizio mail completo

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,12 +1,22 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-var email = prompt("Scrivi la tua email");
+//richiesta email
 var listaEmail = ["claudio@gmail.com", "marco@gmail.com", "paolo@gmail.com", "federico@gmail.com", "luca@gmail.com"];
+var accettata = 0;
+var email = prompt("Scrivi la tua email");
+
+// controllo email
 
 for(i = 0; i < listaEmail.length; i++) {
   if (email == listaEmail[i]) {
-    console.log("Email accettata");
-  } else {
-    console.log("Email non valida")
+    accettata++;
   }
+}
+
+//stampa risposta
+
+if (accettata == 1) {
+  console.log("Email accettata");
+} else {
+  console.log("Email non valida");
 }
